4cf339d6f62f2431bfc8142a0b8c6015a285eb0b
adityagound6 <adityagound6@gmail.com>
2022-12-15 12:57:10 +0530

    Innital

---
+++ src/test/java/com/example/ApnaCab/ApnaCabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApnaCab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApnaCabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
